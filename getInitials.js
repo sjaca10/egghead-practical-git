251398d53a0b4dba7ee0c24b3c64de6c85ae2d1f
Javier Aguila <sjaca10@gmail.com>
2017-05-03 16:34:59 -0500

    Add capitalization and a period to getInitials

--- getInitials.js
+++ getInitials.js
@@ -1,6 +1,6 @@
 // "For your information" -> "FYI"
 function getInitials(name) {
     return name.split(' ')
-        .map(word => word.charAt(0).toUpperCase())
+        .map(word => `${word.charAt(0).toUpperCase()}.`)
         .join('');
 }
